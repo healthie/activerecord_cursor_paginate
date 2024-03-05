4d7dc5c875bcfb239b3a6677887fd1ff64f3a5e1
fatkodima <fatkodima123@gmail.com>
2024-03-05 12:33:17 +0200

    `bundle gem activerecord_cursor_pagination` and cleanup

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in activerecord_cursor_paginate.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/activerecord_cursor_paginate.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "activerecord_cursor_paginate/version"
+
+module ActiveRecordCursorPaginate
+  class Error < StandardError; end
+  # Your code goes here...
+end
